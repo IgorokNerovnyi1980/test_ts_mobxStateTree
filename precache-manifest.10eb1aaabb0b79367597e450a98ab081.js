39b7bd1acd5a94331e4cefbae5b384355bb044c2
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2020-07-26 20:40:06 +0300

    Updates

---
+++ precache-manifest.10eb1aaabb0b79367597e450a98ab081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757a11b1d39ff6e0a8848b0a7a3bc051",
+    "url": "/test_ts_mobxStateTree/index.html"
+  },
+  {
+    "revision": "f91c3ce39e0de623e692",
+    "url": "/test_ts_mobxStateTree/static/js/2.c11600f0.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/test_ts_mobxStateTree/static/js/2.c11600f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72f91ad0e480c889dfc",
+    "url": "/test_ts_mobxStateTree/static/js/main.67733f1e.chunk.js"
+  },
+  {
+    "revision": "bef2f7349dec197eddea",
+    "url": "/test_ts_mobxStateTree/static/js/runtime-main.9650e7fa.js"
+  }
+]);
